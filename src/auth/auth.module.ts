b6925794e2ef1b29cc4c169290a5586b0b4615ac
arshiapiri <arshia.piri8585@gmail.com>
2023-05-31 23:44:12 +0330

    jwt token-based add my project

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
-import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtExpiresIn, jwtSecret } from 'src/config';
+import { AuthController } from './auth.controller';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
+  imports: [
+    JwtModule.register({
+      global: true,
+      secret: jwtSecret,
+      signOptions: { expiresIn: jwtExpiresIn },
+    }),
+    UserModule,
+  ],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,34 @@
+import { MailerService } from '@nestjs-modules/mailer';
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { JwtPayload } from 'jsonwebtoken';
 import { User } from 'src/user/entities/user.entity';
+import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
 export class AuthService {
-  constructor(private jwtService: JwtService) {}
+  constructor(
+    private jwtService: JwtService,
+    private readonly mailerService: MailerService
+    ) {}
 
   async generateJwtToken(user: User): Promise<string> {
     const payload: JwtPayload = { id: user.id };
     return this.jwtService.signAsync(payload);
+  }
+
+  async sendVerificationEmail(email: string, verificationCode: string) {
+    const mailOptions = {
+      to: email,
+      subject: 'Verification Code',
+      text: `Your verification code is: ${verificationCode}`,
+    };
+
+    await this.mailerService.sendMail(mailOptions);
+  }
+  
+  generateVerificationCode(): string {
+    const verificationCode = uuidv4().substr(0, 6);
+    return verificationCode;
   }
 }
