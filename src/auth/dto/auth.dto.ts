f8e641cc3bfb43501b512e07918c24f894e4ea0e
arshiapiri <arshia.piri8585@gmail.com>
2023-05-31 23:45:29 +0330

    dto sign in add

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,3 +1,24 @@
+import { Exclude } from "class-transformer";
+import { User } from "src/user/entities/user.entity";
+
 export class JwtPayload {
     id: number;
+  }
+
+  export class SigninDto {
+    id: number;
+  
+    username: string;
+  
+    email: string;
+  
+    access_token: string;
+  
+    @Exclude()
+    password: string;
+  
+    constructor(partial: Partial<User>, token: string) {
+      Object.assign(this, partial);
+      this.access_token = token;
+    }
   }
