bfdb9debec3e5f8b0eaedf1abf40a659d1720e31
arshiapiri <arshia.piri8585@gmail.com>
2023-05-31 23:46:06 +0330

    delete controller signup in user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -8,14 +8,4 @@ export class UserController {
     constructor(private readonly userService: UserService) {
 
     }
-    @Post("signup")
-    async createUser(@Body() body: CreateUserDto) {
-        if (!!(await this.userService.findUserByEmail(body.email))) {
-            throw new BadRequestException('Try another email.');
-        }
-        if (!!(await this.userService.findUserByUsername(body.username))) {
-            throw new BadRequestException('Try another UserName.');
-        }
-        await this.userService.create(body);
-    }
 }
